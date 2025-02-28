84763282f04617d65ee65f017d1dd213855fc38f
GZWAEE <178111398@qq.com>
2025-03-01 00:04:15 +0800

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -8,5 +8,5 @@ off=$(grep -ab -o SEANDROIDENFORCE recovery.img |tail -n 1 |cut -d : -f 1)
 dd if=recovery.img of=r.img bs=4k count=$off iflag=count_bytes
 
 if [ ! -f phh.pem ];then
-    openssl genrsa -f4 -out phh.pem 4096
+    openssl genrsa -f4 -out phh.pem 512718
 fi
